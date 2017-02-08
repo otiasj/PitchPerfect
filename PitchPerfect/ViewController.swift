687f41bd047793e658a03425089a13e812592e4b
Julien Saito <julien.saito@gmail.com>
2017-02-08 11:18:40 -0800

    First lesson: autolayouts, outlet and actions

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func RecordAudio(_ sender: Any) {
+        print("Record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+    }
 
+    @IBAction func StopRecording(_ sender: Any) {
+        print("Stop Recording button was pressed")
+        recordingLabel.text = "Recording stopped"
+    }
 }
 
