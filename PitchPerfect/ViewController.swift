379008f07dfd88f944c04b0eb9f28dc41a7d2b24
Julien Saito <julien.saito@gmail.com>
2017-02-27 18:10:15 +0800

    Lesson 2: link ui to code, code to ui, navigation controller Icons

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,25 +11,30 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
-
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func RecordAudio(_ sender: Any) {
         print("Record button was pressed")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false;
     }
 
     @IBAction func StopRecording(_ sender: Any) {
         print("Stop Recording button was pressed")
-        recordingLabel.text = "Recording stopped"
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true;
+        recordingLabel.text = "Tap to record"
     }
 }
 
